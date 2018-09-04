43ec04f2f98084cf2055b26c7dcbbb113f75b253
Daeng Rosanda <daengrosanda@gmail.com>
2018-09-05 01:41:40 +0700

    upload manual produk n manual slider

--- app/view/front/page/html/products.php
+++ app/view/front/page/html/products.php
@@ -1,14 +1,14 @@
-
 <div class="row" data-equalizer="produk">
   <!--produk list -->
-  <div class="large-3 medium-4 small-6 columns produk" >
+  <div class="large-3 medium-4 small-6 columns produk">
     <div class="row produk brown" >
       <div class="columns ">
-        <img src="<?=base_url()?>media/products/tie-doctor.jpg" />
+        <img src="<?=base_url()?>media/products/water-law.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/water-law-3.jpg" class="produk-image-hover" />
       </div>
       <div class="columns" data-equalizer-watch="produk">
         <h1 class="produk-title">
-          Casual Tie
+          Water Law
         </h1>
         <p class="produk-price">
           Rp. 100.000
@@ -21,10 +21,95 @@
   </div>
   <!--produk list -->
   <!--produk list -->
-  <div class="large-3 medium-4 small-6 columns produk" >
+  <div class="large-3 medium-4 small-6 columns produk end">
     <div class="row produk brown" >
       <div class="columns ">
-        <img src="<?=base_url()?>media/products/labs-staff.jpg" />
+        <img src="<?=base_url()?>media/products/chopper-pink.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/chopper-pink-3.jpg" class="produk-image-hover" />
+      </div>
+      <div class="columns" data-equalizer-watch="produk">
+        <h1 class="produk-title">
+          Chopper Pink
+        </h1>
+        <p class="produk-price">
+          Rp. 100.000
+        </p>
+      </div>
+      <div class="large-offset-2 large-8 medium-offset-1 medium-10 columns end">
+        <a href="#" class="button expanded tiny">LIHAT</a>
+      </div>
+    </div>
+  </div>
+  <!--produk list -->
+  <!--produk list -->
+  <div class="large-3 medium-4 small-6 columns produk">
+    <div class="row produk brown" >
+      <div class="columns ">
+        <img src="<?=base_url()?>media/products/chopper-bike.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/chopper-bike-3.jpg" class="produk-image-hover" />
+      </div>
+      <div class="columns" data-equalizer-watch="produk">
+        <h1 class="produk-title">
+          Chopper Bike
+        </h1>
+        <p class="produk-price">
+          Rp. 100.000
+        </p>
+      </div>
+      <div class="large-offset-2 large-8 medium-offset-1 medium-10 columns end">
+        <a href="#" class="button expanded tiny">LIHAT</a>
+      </div>
+    </div>
+  </div>
+  <!--produk list -->
+  <!--produk list -->
+  <div class="large-3 medium-4 small-6 columns produk">
+    <div class="row produk brown" >
+      <div class="columns ">
+        <img src="<?=base_url()?>media/products/chopper-dr.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/chopper-dr-3.jpg" class="produk-image-hover" />
+      </div>
+      <div class="columns" data-equalizer-watch="produk">
+        <h1 class="produk-title">
+          Chopper Doctor
+        </h1>
+        <p class="produk-price">
+          Rp. 100.000
+        </p>
+      </div>
+      <div class="large-offset-2 large-8 medium-offset-1 medium-10 columns end">
+        <a href="#" class="button expanded tiny">LIHAT</a>
+      </div>
+    </div>
+  </div>
+  <!--produk list -->
+  <!--produk list -->
+  <div class="large-3 medium-4 small-6 columns produk end">
+    <div class="row produk brown" >
+      <div class="columns ">
+        <img src="<?=base_url()?>media/products/labs-crew.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/labs-crew-3.jpg" class="produk-image-hover" />
+      </div>
+      <div class="columns" data-equalizer-watch="produk">
+        <h1 class="produk-title">
+          Labs Crew
+        </h1>
+        <p class="produk-price">
+          Rp. 100.000
+        </p>
+      </div>
+      <div class="large-offset-2 large-8 medium-offset-1 medium-10 columns end">
+        <a href="#" class="button expanded tiny">LIHAT</a>
+      </div>
+    </div>
+  </div>
+  <!--produk list -->
+  <!--produk list -->
+  <div class="large-3 medium-4 small-6 columns produk end">
+    <div class="row produk brown" >
+      <div class="columns ">
+        <img src="<?=base_url()?>media/products/labs-staff.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/labs-staff-3.jpg" class="produk-image-hover" />
       </div>
       <div class="columns" data-equalizer-watch="produk">
         <h1 class="produk-title">
@@ -41,14 +126,15 @@
   </div>
   <!--produk list -->
   <!--produk list -->
-  <div class="large-3 medium-4 small-6 columns produk">
+  <div class="large-3 medium-4 small-6 columns produk" >
     <div class="row produk brown" >
       <div class="columns ">
-        <img src="<?=base_url()?>media/products/casual.jpg" />
+        <img src="<?=base_url()?>media/products/dasi.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/dasi-3.jpg" class="produk-image-hover" />
       </div>
       <div class="columns" data-equalizer-watch="produk">
         <h1 class="produk-title">
-          Casual Tees
+          Dr Tie
         </h1>
         <p class="produk-price">
           Rp. 100.000
@@ -61,14 +147,36 @@
   </div>
   <!--produk list -->
   <!--produk list -->
-  <div class="large-3 medium-4 small-6 columns produk">
+  <div class="large-3 medium-4 small-6 columns produk" >
     <div class="row produk brown" >
       <div class="columns ">
-        <img src="<?=base_url()?>media/products/chopper-bike.jpg" />
+        <img src="<?=base_url()?>media/products/keep-calm.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/keep-calm-3.jpg" class="produk-image-hover" />
       </div>
       <div class="columns" data-equalizer-watch="produk">
         <h1 class="produk-title">
-          Chopper Bike
+          Keep Calm
+        </h1>
+        <p class="produk-price">
+          Rp. 100.000
+        </p>
+      </div>
+      <div class="large-offset-2 large-8 medium-offset-1 medium-10 columns end">
+        <a href="#" class="button expanded tiny">LIHAT</a>
+      </div>
+    </div>
+  </div>
+  <!--produk list -->
+  <!--produk list -->
+  <div class="large-3 medium-4 small-6 columns produk end">
+    <div class="row produk brown" >
+      <div class="columns ">
+        <img src="<?=base_url()?>media/products/stetoskop.jpg" class="produk-image-active" />
+        <img src="<?=base_url()?>media/products/stetoskop-3.jpg" class="produk-image-hover" />
+      </div>
+      <div class="columns" data-equalizer-watch="produk">
+        <h1 class="produk-title">
+          Stethoscope
         </h1>
         <p class="produk-price">
           Rp. 100.000
--- app/view/front/page/html/sliders.php
+++ app/view/front/page/html/sliders.php
@@ -1,8 +1,17 @@
 <div class="row" style="margin-top: 0.5em;">
   <div class="large-12 columns">
-    <ul class="orbit-container">
+    <ul class="orbit-container" data-orbit>
       <li class="orbit-slide is-active">
-        <img class="orbit-image" src="<?=base_url()?>media/sliders/banner-001.jpg" alt="Space">
+        <img class="orbit-image" src="<?=base_url()?>media/sliders/banner-001.jpg" alt="Dr Vice">
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="<?=base_url()?>media/sliders/banner-002.jpg" alt="Collection">
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="<?=base_url()?>media/sliders/banner-003.jpg" alt="Casual">
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="<?=base_url()?>media/sliders/banner-004.jpg" alt="Semi Formal">
       </li>
     </ul>
   </div>
