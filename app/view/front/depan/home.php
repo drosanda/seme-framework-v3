ad218e3406f91486eefb896347b15113e98aa67a
Daeng Rosanda <DaengRosanda@Gmail.com>
2020-03-02 15:03:40 +0700

    Menambah view.

--- app/view/front/depan/home.php
+++ app/view/front/depan/home.php
@@ -1,17 +1,34 @@
-<div class="row" style="background-image: url('<?=base_url('media/sliders/tangan.jpg')?>');" />
+<style>
+.container{
+  background-color: #FAA8D2;
+}
+</style>
+
+<div class="row">
   <div class="col-md-2">
-    <h3>Tanggal</h3>
+    <h3 ><a href="">Tanggal!</a></h3>
   </div>
   <div class="col-md-2">
-    <h3>Waktu</h3>
+    <h3><a href="">Waktu!</a></h3>
   </div>
   <div class="col-md-2">
-    <h3>Tempat</h3>
+    <h3><a href="">Tempat!</a></h3>
+
   </div>
   <div class="col-md-2">
-    <h3>Gallery</h3>
+    <h3><a href="">Gallery!</a></h3>
+
   </div>
   <div class="col-md-2">
-    <h3>Lokasi</h3>
+    <h3><a href="">Tempat</a></h3>
+  </div>
+</div>
+<br>
+<br>
+<div class="row">
+  <div class="col-md-12">
+    <div class="row" style="background-image: url('<?=base_url('media/sliders/tangan.jpg')?>');" />
+      <h1>Selamat datang di pernikahan kami</h1>
+    </div>
   </div>
 </div>
