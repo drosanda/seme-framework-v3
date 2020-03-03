79036cc7b40fdc7c3d595f4158c3c7d91d2a2982
Daeng Rosanda <DaengRosanda@Gmail.com>
2020-03-03 13:12:03 +0700

    Menambah polder waktu/tempat/tentang.

--- app/view/front/depan/home.php
+++ app/view/front/depan/home.php
@@ -18,23 +18,47 @@
 .peta-lokasi{
   font-style: italic;
 }
+.md{
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom:25px;
+  padding-top:50px;
+}
+.wi{
+  padding-left: 170px;
+  padding-right: 50px;
+  padding-bottom:25px;
+  padding-top:50px;
+}
+.dn{
+  padding-left: 200px;
+  padding-right: 50px;
+  padding-bottom:25px;
+  padding-top:50px;
+}
 </style>
-
 <div class="row">
   <div class="col-md-1 tentang">
-    <h3 ><a href="">Tentang!</a></h3>
+    <h3 ><a href="tentang">Tentang!</a></h3>
   </div>
   <div class="col-md-1 waktu">
-    <h3><a href="">Waktu!</a></h3>
+    <h3><a href="waktu">Waktu!</a></h3>
   </div>
   <div class="col-md-1 tempat">
-    <h3><a href="">Tempat!</a></h3>
+    <h3><a href="tempat">Tempat!</a></h3>
   </div>
   <div class="col-md-1 gallery">
-    <h3><a href="">Gallery!</a></h3>
+    <h3><a href="gallery">Gallery!</a></h3>
   </div>
   <div class="col-md-2 peta-lokasi">
-    <h3><a href="">Peta Lokasi!</a></h3>
+    <h3><a href="peta-lokasi">Peta Lokasi!</a></h3>
+  </div>
+</div>
+<div class="row md">
+  <div class="col-md-12">
+    <h1 class="wi"></h1>
+    <h1 class="dn"></h1>
+    <h1 class="md"></h1>
   </div>
 </div>
 <br>
--- app/view/front/tentang/home.php
+++ app/view/front/tentang/home.php
@@ -3,38 +3,23 @@
   width: 100%;
   font-size: 20px;
 }
-.kevin{
+.tuhan{
+  padding-left: 200px;
   color:#000000;
-  padding-left: 100px;
 }
-.widiani{
-  padding-left: 570px;
-  color:#ffffff;
-  font-size: 38px;
-}
-.atkonah{
-  padding-left: 500px;
-  color:#ffffff;
-  font-size: 38px;
-}
-.dan{
-  color:#ffffff;
-  font-size: 38px;
-  padding-left: 600px;
+.penutupan{
+  color:#000000;
 }
 </style>
 <div class="row">
   <div class="col-md-12 ">
       <p class="kevin">Ketika tuhan menulis takdirnya Tuhan juga menulis satu nama dalam hidupku Bertahun aku menunggu satu nama itu</p>
       <p class="kevin">Akhirnya hari ini terjawab sudah penantian itu Kamu adalah takdirku</p>
-      <h2 class="widiani">WIDIANI</h2>
-      <h2 class="dan">DAN</h2>
-      <h2 class="atkonah">AAT ATKONAH, A. md</h2>
     </div>
   </div>
   <br>
   <div class="row">
-    <div class="col-md-6 ">
+    <div class="col-md-6 tuhan">
       <p>Tuhan membuat segala sesuatu</P>
         <p>Indah pada waktunya</p>
         <p>INdah saat ia mempertemukan kami</p>
@@ -43,7 +28,7 @@
         <p>Dalam pernikahan kudus pada :</p>
 
       </div>
-      <div class="col-md-6 ">
+      <div class="col-md-6 tuhan">
         <p>Kebahagian akami akan terasa lebih sempurna dengan</p>
         <p>Kehadiran dan da restu Bapak/Ibu/Saudara/Saudari</p>
         <p>Dalam acara Ucapan syukur Pernikahan yang akan </P>
@@ -52,13 +37,13 @@
         </div>
         <br>
         <div class="row">
-          <div class="col-md-6 sambutan">
+          <div class="col-md-6 penutupan">
             <p><b>Sabtu, 14 Februari 2015</b></p>
             <p><b>Pukul 10:30 WIB</b></p>
             <p><b>Bertempat di GMIBM Paulus Pusian</b></p>
             <p><b>Desa Pusian Kec. Dumago</b></p>
           </div>
-          <div class="col-md-6 prnutupan">
+          <div class="col-md-6 penutupan">
             <p><b>Sabtu, 14 Februari 2015</b></p>
             <p><b>Pukul 10:30 WIB</b></p>
             <p><b>Bertempat di GMIBM Paulus Pusian</b></p>
