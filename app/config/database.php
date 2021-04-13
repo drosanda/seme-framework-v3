0f35270d7f2d01c5a444351799cc2eb208374d5f
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-04-13 13:47:30 +0700

    FEAT: add charset configuration

--- app/config/database.php
+++ app/config/database.php
@@ -4,3 +4,14 @@ $db['user']  = "root";
 $db['pass']  = "";
 $db['name']  = "seme_framework";
 $db['engine']= "mysqli"; //available mysql,mysqli,pdo
+$db['port'] = '3306';
+$db['charset'] = 'utf8mb4';
+$db['dbcollate'] = '';
+
+/**
+ * Available options:
+ *  - development
+ *  - production
+ * @var string
+ */
+$db['environment'] = 'development';
